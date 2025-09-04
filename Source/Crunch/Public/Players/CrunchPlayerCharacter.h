470c0dfa166a96c1e38e82ae7160cec6839f7723
Chalat Srisuwan <sakichan2521@gmail.com>
2025-09-04 14:55:50 +0700

    Game Mode, Characters and Controller Added.

---
+++ Source/Crunch/Public/Players/CrunchPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Players/CrunchCharacter.h"
+#include "CrunchPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API ACrunchPlayerCharacter : public ACrunchCharacter
+{
+	GENERATED_BODY()
+	
+};
